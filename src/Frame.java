a3b636a1f5c1be523cbd50f47753704c4d3904ac
Abdullah Afif Habiburrohman <abdullahafifhabiburrohman@mail.ugm.ac.id>
2025-05-11 10:01:12 +0700

    Finish Frame.java

--- src/Frame.java
+++ src/Frame.java
@@ -1,5 +1,29 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Objects;
+
 public class Frame {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        JFrame frame = new JFrame();
+
+        frame.setVisible(true);
+        frame.setSize(500,500);
+        frame.setTitle("Frame.java");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+
+        ImageIcon icon = new ImageIcon(Objects.requireNonNull(Frame.class.getResource("/resource/circular.png")));
+        frame.setIconImage(icon.getImage());
+
+        frame.getContentPane().setBackground(Color.green);
+
+//        public class MyFrame extends JFrame{
+//            MyFrame(){
+//                Change frame. to this.
+//                then to psvm add MyFrame myFrame = new MyFrame();
+//            }
+//        }
+
     }
 }
