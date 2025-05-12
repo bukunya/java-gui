c307aa3880cd98d9d599a6056e4143a38ce902b1
Abdullah Afif Habiburrohman <abdullahafifhabiburrohman@mail.ugm.ac.id>
2025-05-12 13:01:58 +0700

    Finished Grid.java

---
+++ src/Grid.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.Button;
+
+public class Grid {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.setLayout(new GridLayout(3,3,10,10));
+
+        frame.add(new JButton("1"));
+        frame.add(new JButton("2"));
+        frame.add(new JButton("3"));
+        frame.add(new JButton("4"));
+        frame.add(new JButton("5"));
+        frame.add(new JButton("6"));
+        frame.add(new JButton("7"));
+
+        frame.setVisible(true);
+    }
+}
