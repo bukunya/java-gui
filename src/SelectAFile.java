08e735ec9de12f53eb8a6cfc90c20b0333b5f035
Abdullah Afif Habiburrohman <abdullahafifhabiburrohman@mail.ugm.ac.id>
2025-05-13 12:57:06 +0700

    Finished SelectAFile.java

---
+++ src/SelectAFile.java
@@ -0,0 +1,39 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+class MyFrame6 extends JFrame implements ActionListener {
+
+    JButton button;
+
+    MyFrame6(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(500,500);
+
+        button = new JButton("Select file");
+        button.addActionListener(this);
+
+        this.add(button);
+        this.pack();
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == button){
+            JFileChooser fileChooser = new JFileChooser();
+            int response = fileChooser.showOpenDialog(null); //showSaveDialog to open save
+            if (response == JFileChooser.APPROVE_OPTION) {
+                File file = new File(fileChooser.getSelectedFile().getAbsolutePath());
+                System.out.println(file);
+            }
+        }
+    }
+}
+
+public class SelectAFile {
+    public static void main(String[] args) {
+        new MyFrame6();
+    }
+}
